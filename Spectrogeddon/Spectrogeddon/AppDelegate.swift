a1be62e8cb5027db25ca338713937f7a9a4eb19b
Tom York <git@zubenelgenubi.org.uk>
2019-05-11 19:56:33 +0100

    iOS project - Add swift support and convert the app delegate

---
+++ Spectrogeddon/Spectrogeddon/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Spectrogeddon
+//
+//  Created by Tom York on 11/05/2019.
+//  Copyright © 2019 Random. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+ 
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        application.isIdleTimerDisabled = true
+        return true
+    }
+}
